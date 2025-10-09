54ed4b496f0ea59bd05decf03910d7eeccaadb14
Suisan-neki <b241651@hiroshima-u.ac.jp>
2025-10-09 12:46:34 +0900

    荳崎ｦ√↑襍ｷ蜍輔せ繧ｯ繝ｪ繝励ヨ繧貞炎髯､縺励《tart_systems.bat繧堤ｰ｡貎泌喧 (38陦娯・7陦・

--- start_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0SecHack365_project\info_sharing_system"
-python run_app.py
-pause
--- start_server.ps1
+++
@@ -1,3 +0,0 @@
-# PowerShell用起動スクリプト
-Set-Location -Path "$PSScriptRoot\SecHack365_project\info_sharing_system"
-python run_app.py
